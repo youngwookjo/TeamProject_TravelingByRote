f2af9eb585c1e0c2b4d8536ee4621209d47716c2
youngwookjo <wwwe2410@naver.com>
2019-10-14 18:40:03 +0900

    Revert "feat : add front,login"
    This reverts commit 473c6c128dacecd2d6d87279b5a239a3b9a096b8.

--- tbr/src/main/java/tbr/model/dao/PlaceRepository.java
+++ tbr/src/main/java/tbr/model/dao/PlaceRepository.java
@@ -1,7 +1,6 @@
 package tbr.model.dao;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,8 +9,5 @@ import tbr.model.dto.Place;
 
 @Repository
 public interface PlaceRepository extends CrudRepository<Place, BigDecimal>{
-	
-	public List<Place> findPlaceByNameContainingOrderByTotalDesc(String name);
-	
 	
 }
