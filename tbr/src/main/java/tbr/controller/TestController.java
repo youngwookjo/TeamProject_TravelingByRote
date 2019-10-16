5403bec9692898d401abc72a3873831065124907
seyoungnam <seyoung.nam@gmail.com>
2019-10-16 17:58:47 +0900

    fix: user CRUD 링크 버그 수정

--- tbr/src/main/java/tbr/controller/TestController.java
+++ tbr/src/main/java/tbr/controller/TestController.java
@@ -3,11 +3,9 @@ package tbr.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,174 +14,125 @@ import tbr.model.dto.TbrUser;
 import tbr.service.PlaceService;
 import tbr.service.UserService;
 
-@CrossOrigin(origins= {"http://127.0.0.1:8000", "http://localhost:8000"})
+@CrossOrigin(origins = { "http://127.0.0.1:8000", "http://localhost:8000" })
 @RestController
 public class TestController {
 	@Autowired
 	UserService userService;
 	@Autowired
 	PlaceService placeService;
-	
-	//0. 로그인
-		//http://localhost:8000/addUser?id=tester1&pw=secretkey1
-		//http://localhost:8000/addUser?id=tester2&pw=secretkey2
-/*		@GetMapping("/login/loginUser")
-		public String loginUser(@RequestParam("id") String id, @RequestParam("pw") String pw, Model model) {
-	
-			TbrUser userInfo = new TbrUser(id, pw);
-			String result = "로그인 실패";
-			boolean flag = userService.login(userInfo);
-			if (flag == true) {
-				result = "로그인 성공";
-			}
-			System.out.println(result);
-			model.addAttribute("object", "userInfo");
-			return "redirect:/andrea/index.html";
-		}*/
-	
-//		@RequestMapping(value="/login/loginUser", method=RequestMethod.POST)
-//		public String loginUser(@RequestParam("id") String id, @RequestParam("pw") String pw, Model model){
-//			
-//			TbrUser userInfo = new TbrUser(id, pw);
-//			String result = "로그인 실패";
-//			boolean flag = userService.login(userInfo);
-//	        if (flag == true) {
-//	        	result = "로그인 성공";
-//	        }
-//	        System.out.println(result);
-//	        model.addAttribute("userInfo", userInfo);
-//	        System.out.println(model.toString());
-//	        return "andrea/index.html";
-//		}
-		
-		@PostMapping("/loginUser")
-		public ModelAndView loginUser(@RequestParam("id") String id, @RequestParam("pw") String pw, ModelAndView mv){
-			TbrUser userInfo = new TbrUser(id, pw);
-			String result = "로그인 실패";
-			boolean flag = userService.login(userInfo);
-	        if (flag == true) {
-	        	result = "로그인 성공";
-	        }
-	        System.out.println(result);
-	        mv.setViewName("../static/index.html");
-	        mv.addObject("userInfo", userInfo);
-	     
-	        return mv;
+
+	// 1. User : 로그인
+	@PostMapping("/loginUser")
+	public ModelAndView loginUser(@RequestParam("id") String id, @RequestParam("pw") String pw, ModelAndView mv) {
+		TbrUser userInfo = new TbrUser(id, pw);
+		String result = "로그인 실패";
+		boolean flag = userService.login(userInfo);
+		if (flag == true) {
+			result = "로그인 성공";
 		}
-		
-/*		@GetMapping("/login/loginUser")
-		public ModelAndView loginUser(@RequestParam("id") String id, @RequestParam("pw") String pw){
-			TbrUser userInfo = new TbrUser(id, pw);
-			String result = "로그인 실패";
-			boolean flag = userService.login(userInfo);
-	        if (flag == true) {
-	        	result = "로그인 성공";
-	        }
-	        System.out.println(result);
-	        return new ModelAndView("redirect:/andrea/index.html", "userInfo", userInfo);
-		}*/
-	
-	//1. 회원가입
-	//http://localhost:8000/addUser?id=tester1&pw=secretkey1
-	//http://localhost:8000/addUser?id=admin&pw=admin
-	@GetMapping("/addUser")
-	public ModelAndView addUser(@RequestParam("id") String id, @RequestParam("pw") String pw, ModelAndView mv){
+		System.out.println(result);
+		mv.setViewName("../static/index.html");
+		mv.addObject("userInfo", userInfo);
+
+		return mv;
+	}
+
+	// 2. User : 회원가입
+	@PostMapping("/addUser")
+	public ModelAndView addUser(@RequestParam("id") String id, @RequestParam("pw") String pw, ModelAndView mv) {
 		TbrUser userInfo = new TbrUser(id, pw);
 		String result = "가입 실패";
 		boolean flag = userService.add(userInfo);
-        if (flag == true) {
-        	result = "가입 성공";
-        }
-        System.out.println(result);
-        mv.setViewName("../static/login.html");
-        mv.addObject("userInfo", userInfo);
-        return mv;
+		if (flag == true) {
+			result = "가입 성공";
+		}
+		System.out.println(result);
+		mv.setViewName("../static/login.html");
+		mv.addObject("userInfo", userInfo);
+		return mv;
 	}
-	
 
-	
-	//3. 회원정보 조회(Equal)
-	@RequestMapping("/getUser")
-	public TbrUser getUser(@RequestParam("id") String id){
+	// 3. User : 개인정보 조회(아직 미구현)
+	@PostMapping("/getUser")
+	public TbrUser getUser(@RequestParam("id") String id) {
 		TbrUser result = userService.get(id);
 		System.out.println(result.toString());
 		return result;
 	}
 
-
-	//4. 회원 pw 수정
-	//http://localhost:8000/updateUser?id=tester1&pw=revised
-	@GetMapping("/updateUser")
-	public String updateUser(@RequestParam("id") String id, @RequestParam("pw") String pw, Model model) {
+	// 4. User : 회원 비밀번호 수정
+	@PostMapping("/updateUser")
+	public ModelAndView updateUser(@RequestParam("id") String id, @RequestParam("pw") String pw, ModelAndView mv) {
 		try {
 			TbrUser userInfo = new TbrUser(id, pw);
 			userService.update(userInfo);
-			model.addAttribute("userInfo", userInfo);
-		}catch(Exception e) {
+			mv.setViewName("../static/about.html");
+			mv.addObject("userInfo", userInfo);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "redirect:/contact.html";
-	
+		return mv;
+
 	}
-	
-	//5. 회원 탈퇴
-	@RequestMapping("/deleteUser")
-	//http://localhost:8000/deleteUser?id=tester1&pw=secretkey1
-	public String deleteUser(@RequestParam("id") String id, @RequestParam("pw") String pw) {
-		TbrUser info = new TbrUser(id, pw);
+
+	// 5. User : 회원 탈퇴 (아직 미구현)
+	@PostMapping("/deleteUser")
+	// http://localhost:8000/deleteUser?id=tester1&pw=secretkey1
+	public ModelAndView deleteUser(@RequestParam("id") String id, @RequestParam("pw") String pw, ModelAndView mv) {
+		TbrUser userInfo = new TbrUser(id, pw);
 		try {
-			userService.delete(info);
-		}catch(Exception e) {
+			userService.delete(userInfo);
+			mv.setViewName("../static/login.html");
+			mv.addObject("userInfo", userInfo);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("회원 탈퇴 완료");
-        return "회원 탈퇴 완료";
+		return mv;
 	}
-	
-	//1. admin page : 회원정보 조회(전체)
-	//http://localhost:8000/getAllUser
+
+	// 6. Admin : 회원정보 조회(전체)
 	@GetMapping("/getAllUser")
 	public Iterable<TbrUser> getAllUser() {
 		Iterable<TbrUser> list = userService.getAll();
 		System.out.println(list.toString());
 		return list;
-	}   
-	
-	//2. Admin page : 회원정보 조회(Containing)
-	//http://localhost:8000/getUser/tester1
-	//http://localhost:8000/getUser/tester3
+	}
+
+	// 7. Admin : 회원정보 조회(Containing)
 	@GetMapping("/searchAccount")
-	public List<TbrUser> searchAccount(@RequestParam("id") String id){
+	public List<TbrUser> searchAccount(@RequestParam("id") String id) {
 		List<TbrUser> list = userService.searchId(id);
 		System.out.println(list.toString());
 		return list;
 	}
-	
-	//3. admin page : 회원 삭제
+
+	// 8. admin page : 회원 삭제
 	@GetMapping("/deleteAccount")
-	//http://localhost:8000/deleteAccount?id=tester1
+	// http://localhost:8000/deleteAccount?id=tester1
 	public String deleteAccount(@RequestParam("id") String id) {
 		try {
 			userService.deleteId(id);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("회원 삭제 완료");
-        return "회원 삭제 완료";
+		return "회원 삭제 완료";
 	}
-	
+
 	@PostMapping("/loginAdmin")
-	public ModelAndView loginAdmin(@RequestParam("id") String id, @RequestParam("pw") String pw, ModelAndView mv){
+	public ModelAndView loginAdmin(@RequestParam("id") String id, @RequestParam("pw") String pw, ModelAndView mv) {
 		TbrUser userInfo = new TbrUser(id, pw);
 		String result = "어드민 로그인 실패";
 		boolean flag = userService.adlogin(userInfo);
-        if (flag == true) {
-        	result = "어드민 로그인 성공";
-        }
-        System.out.println(result);
-        mv.setViewName("../static/admain.html");
-        mv.addObject("userInfo", userInfo);
-     
-        return mv;
+		if (flag == true) {
+			result = "어드민 로그인 성공";
+		}
+		System.out.println(result);
+		mv.setViewName("../static/admain.html");
+		mv.addObject("userInfo", userInfo);
+
+		return mv;
 	}
 }
